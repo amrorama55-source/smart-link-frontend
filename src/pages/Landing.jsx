b4478db99beeaf4113655f2fc6165ee425b3eddd
amrorama55 <amrorama55@gmail.com>
2026-05-03 15:42:23 +0300

    Add Trustpilot review widget for credibility

--- src/pages/Landing.jsx
+++ src/pages/Landing.jsx
@@ -771,6 +771,13 @@ export default function LandingPage() {
               </motion.div>
             ))}
           </div>
+
+          <div className="mt-16 flex justify-center">
+            {/* TrustBox widget - Mini */}
+            <div className="trustpilot-widget" data-locale="en-US" data-template-id="53aa8807dec7e10d38f59f32" data-businessunit-id="69f7390bebbd3c000d06bf18" data-style-height="150px" data-style-width="100%" data-token="b67fd1c8-4724-4922-ae25-038862ea786a">
+              <a href="https://www.trustpilot.com/review/by-smartlink.com" target="_blank" rel="noopener noreferrer">Trustpilot</a>
+            </div>
+          </div>
         </div>
       </section>
 
